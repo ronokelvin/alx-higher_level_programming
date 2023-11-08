a27b267461484763f089ac73c55b7186ffe41476
ronokelvin <ronokipyegon@gmail.com>
2023-11-08 13:32:00 -0800

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+
+
+def number_keys(a_dictionary):
+    """
+    Count and return the number of keys in a dictionary.
+
+    :param a_dictionary: The dictionary to count keys from.
+    :return: The number of keys in the dictionary.
+    """
+    return len(a_dictionary)
